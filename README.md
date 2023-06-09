4773fec67a896b41cbba64ae26116c1d21d31d55
sayumani <sayoojvylappully@gmail.com>
2023-06-09 11:56:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-3bwwqd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-3bwwqd)
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Checkbox from '@mui/material/Checkbox';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import './style.css';
+
+export default function App() {
+  return (
+    <FormControl>
+      <FormLabel>tets gfgfg ghhggh gfhghfghgfhff</FormLabel>
+      <Checkbox />
+    </FormControl>
+  );
+}
